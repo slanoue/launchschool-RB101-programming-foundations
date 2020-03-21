4e10c33d268365baadff289fb190c161a13cf0b8
Spencer Lanoue <splanoue@gmail.com>
2020-03-21 10:24:14 -0700

    Notes from the lesson 2 kickoff lecture

---
+++ 2_small_programs/kickoff_lecture_notes.rb
@@ -0,0 +1,21 @@
+# Goal of these lessons:
+# * Build fluency in language syntax
+# * Learn to think like a programmer
+# * Learn to build web applications
+# * Debugging mindset
+
+# We'll cover a lot of areas, but you can categorize them into 3 main buckets:
+# * Learning to program procedurally (e.g. grammar/syntax)
+# * Object oriented programming
+# * Other stuff (e.g. testing, blocks, tools, problem solving, developing familiarity with the language)
+
+# Learning to code vs. learning to program:
+# * First, Ruby grammar & syntax (e.g. English grammar/vocabulary)
+# * Second, building applications (e.g. writing essays/novels)
+# * Very frustrating to jump between the two when you're first learning
+# * Use pseudo code to help with this challenge
+
+# Looking for help:
+# * Search using the "right phrases" (Use the stack trace, read error messages, add "ruby" to your search)
+# * Stack Overflow
+# * Ruby docs (not Rails docs)
