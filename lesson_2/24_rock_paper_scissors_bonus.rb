c7edf171c18469b915b89f68417c49bde2ab7784
Spencer Lanoue <splanoue@gmail.com>
2020-04-08 06:11:46 -0700

    First draft of rock paper scissors bonus logic

--- lesson_2/24_rock_paper_scissors_bonus.rb
+++ lesson_2/24_rock_paper_scissors_bonus.rb
@@ -1,37 +1,14 @@
-# VALID_CHOICES = {
-#   'r' => 'rock',
-#   'p' => 'paper',
-#   'sc' => 'scissors',
-#   'sp' => 'spock',
-#   'l' => 'lizard'
-# }
-
-WINNING_CONDITIONS = {
-  'rock' => ['scissors', 'lizard'],
-  'paper' => ['rock', 'spock'],
-  'scissors' => ['paper', 'lizard'],
-  'spock' => ['scissors', 'rock'],
-  'lizard' => ['spock', 'paper']
+VALID_CHOICES = {
+  'r' => 'rock',
+  'p' => 'paper',
+  'sc' => 'scissors',
+  'sp' => 'spock',
+  'l' => 'lizard'
 }
 
-# def prompt(message)
-#    puts "=> #{message}"
-# end
-
-# def display_instructions
-#   puts ""
-#   prompt("Welcome to #{VALID_CHOICES.values.join(', ')}!")
-#   prompt("Here's how it works:")
-#   puts ""
-#
-#   WINNING_CONDITIONS.each { |k, v| prompt("👉 #{k.capitalize} beats #{v[0]} and #{v[1]}") }
-#
-#   puts ""
-#   prompt("Each time you win, you get 1 point.")
-#   prompt("First player to 5 points becomes the Grand Winner!")
-#   prompt("Now, let's get started...")
-#   puts ""
-# end
+def prompt(message)
+   puts "=> #{message}"
+end
 
 def win?(first, second)
   (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
@@ -52,75 +29,54 @@ def display_results(player, computer)
 end
 
 score = {
-  "player" => 0,
-  "computer" => 0
+  'player' => 0,
+  'computer' => 0
 }
 
-def keep_score(score, player, computer)
-  if win?(player, computer)
-    score["player"] += 1
-  elsif win?(computer, player)
-    score["computer"] += 1
-  end
-
-  prompt("<Score>")
-  score.each { |k, v| prompt("#{key}: #{value}") }
-end
-
-display_instructions()
-
 loop do
-  user_choice = ''
+  player_choice = ''
 
   loop do
-    # prompt("Choose one:")
-    # puts ""
-    # VALID_CHOICES.each { |k, v| prompt("👉 #{v.capitalize} (or #{k})") }
-    user_choice = gets.chomp.downcase
+    prompt("Choose one: #{VALID_CHOICES.values.join(', ')}")
 
-    if VALID_CHOICES.values.include?(user_choice)
+    player_choice = gets.chomp.downcase
+
+    if VALID_CHOICES.keys.include?(player_choice)
+      player_choice = VALID_CHOICES[player_choice]
       break
-    elsif VALID_CHOICES.keys.include?(user_choice)
-      case
-        when user_choice == 'r'
-          user_choice = 'rock'
-        when user_choice == 'p'
-          user_choice = 'paper'
-        when user_choice == 'sc'
-          user_choice = 'scissors'
-        when user_choice == 'sp'
-          user_choice = 'spock'
-        when user_choice == 'l'
-          user_choice = 'lizard'
-      end
+    elsif VALID_CHOICES.values.include?(player_choice)
       break
     else
-      puts ""
-      prompt("🚫 That's not a valid choice. Please try again")
+      prompt("That's not a valid choice.")
     end
-
   end
 
   computer_choice = VALID_CHOICES.values.sample
 
-  display_results(user_choice, computer_choice)
+  prompt("You chose #{player_choice}, the computer chose #{computer_choice}")
 
-  keep_score(score, player, computer)
+  if win?(player_choice, computer_choice)
+    puts "you win!"
+    score['player'] += 1
+    puts score['player']
+  elsif win?(computer_choice, player_choice)
+    puts "computer wins!"
+    score['computer'] += 1
+    puts score['computer']
+  else
+    puts "it's a tie!"
+  end
 
+  break if score.value?(5)
 
+  # display_results(player_choice, computer_choice)
 
+  # prompt("<Scores>")
+  # score.each { |k, v| prompt("#{k.capitalize}: #{v}") }
 
-
-
-
-
-
-  # - Tally the score
-  # - Display the winner and the new score
-
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?("y")
+  # prompt("Do you want to play again?")
+  # answer = gets.chomp
+  # break unless answer.downcase.start_with?("y")
 end
 
 prompt("Thank you for playing! Goodbye")
