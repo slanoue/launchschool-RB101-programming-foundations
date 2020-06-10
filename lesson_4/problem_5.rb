fce102a5bf8c1bc3c3138f2a655ae11b847a860b
Spencer Lanoue <splanoue@gmail.com>
2020-06-10 06:43:48 -0700

    lesson 4 problem set solutions

---
+++ lesson_4/problem_5.rb
@@ -0,0 +1,7 @@
+# In the array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Find the index of the first name that starts with "Be"
+
+flintstones.index { |name| name[0, 2] == "Be"}
